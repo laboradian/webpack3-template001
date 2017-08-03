cfdaae860bf119880102d184da79fbd6f76be5a4
kura5 <laboradian@gmail.com>
2017-08-03 15:32:32 +0900

    Add a test file

---
+++ test/main.js
@@ -0,0 +1,26 @@
+import assert from 'power-assert';
+//import myMathModule from '../src/js/myMathModule';
+//import { Person } from '../src/js/Person.js';
+//
+//describe('module myMathModule', () => {
+//  describe('method multiplyBy2', () => {
+//    it ('multiply some values by 2', () => {
+//      assert(myMathModule.multiplyBy2(0) === 0);
+//      assert(myMathModule.multiplyBy2(2) === 4);
+//      assert(myMathModule.multiplyBy2(3) === 6);
+//    });
+//  });
+//});
+//
+//describe('class Person', () => {
+//  describe('function greet', () => {
+//    it ('greet using its name', () => {
+//      let p = new Person('foo');
+//      assert(p.greet() === "私は、foo と言います。");
+//    });
+//    it ('greet using empty name', () => {
+//      let p = new Person('');
+//      assert(p.greet() === "私は、 と言います。");
+//    });
+//  });
+//});
